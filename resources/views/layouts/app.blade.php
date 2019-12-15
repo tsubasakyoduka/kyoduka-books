7b003aa2f987f841baf0819fc713a3054f84ec3e
tsubasa kyouduka <tubasa.0312@outlook.jp>
2019-12-15 11:16:33 +0000

    background

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,12 +7,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
-        <style>
-            body{
-                background-image: url(../baby.jpg);
-                background-size: cover;
-            }
-        </style>
+       
+        
+       
     </head>
 
     <body>
--- resources/views/users/followings.blade.php
+++ resources/views/users/followings.blade.php
@@ -7,7 +7,7 @@
         </aside>
         <div class="col-sm-8">
             <ul class="nav nav-tabs nav-justified mb-3">
-                <li class="nav-item"><a href="{{ route('users.show', ['id' => $user->id]) }}" class="nav-link {{ Request::is('users/' . $user->id) ? 'active' : '' }}">ポートフォリオ<span class="badge badge-secondary">{{ $count_mybooks }}</span></a></li>
+                <li class="nav-item"><a href="{{ route('users.show', ['id' => $user->id]) }}" class="nav-link {{ Request::is('users/' . $user->id) ? 'active' : '' }}">Portfolio<span class="badge badge-secondary">{{ $count_mybooks }}</span></a></li>
     <li class="nav-item"><a href="{{ route('users.followings', ['id' => $user->id]) }}" class="nav-link {{ Request::is('users/*/followings') ? 'active' : '' }}">フォロー<span class="badge badge-secondary">{{ $count_followings }}</span></a></li>
     <li class="nav-item"><a href="{{ route('users.followers', ['id' => $user->id]) }}" class="nav-link {{ Request::is('users/*/followers') ? 'active' : '' }}">フォロワー<span class="badge badge-secondary">{{ $count_followers }}</span></a></li>
     <li class="nav-item"><a href="{{ route('favorites.favoritings', ['id' => $user->id]) }}" class="nav-link {{ Request::is('mybooks/*/favoritings') ? 'active' : '' }}">お気に入り<span class="badge badge-secondary">{{ $count_favoritings }}</span></a></li>
