086bbdc7acc2fc70f42f7e50bb1a887e96f5d45b
tsubasa kyouduka <tubasa.0312@outlook.jp>
2019-12-16 05:10:32 +0000

    newcomit

--- resources/views/mybooks/edit.blade.php
+++ resources/views/mybooks/edit.blade.php
@@ -2,20 +2,20 @@
 
 @section('content')
 
-    <h1>本の内容を編集する</h1>
+    <h1>編集ページ</h1>
 
     <div class="row">
         <div class="col-4">
-             <h6>本の写真：</h6>
+             <h6>写真：</h6>
                 <img class="rounded img-fluid" src="{{ Gravatar::src(Auth::user()->email, 500) }}" alt="">
             
                     {!! Form::model($mybook, ['route' => ['mybooks.update', $mybook->id], 'method' => 'put']) !!}
                         <div class="form-group">
-                            {!! Form::label('content', '本の名前:') !!}
+                            {!! Form::label('content', 'URL:') !!}
                             {!! Form::text('title', null, ['class' => 'form-control']) !!}
-                            {!! Form::label('content', 'この本で得た事:') !!}
+                            {!! Form::label('content', '内容:') !!}
                             {!! Form::textarea('content', old('content'), ['class' => 'form-control', 'rows' => '2']) !!}
-                            {!! Form::submit('本棚に入れる', ['class' => 'btn btn-success btn-block']) !!}
+                            {!! Form::submit('編集を完了する', ['class' => 'btn btn-success btn-block']) !!}
                         </div>
                     {!! Form::close() !!}
             
